6c709b389fca5c3c8553073cbaff2209464383e9
Sofxx7 <octubreok84@gmail.com>
2026-05-08 13:53:02 -0500

    feat: initialize ASP.NET Core Web API project and CRUD controller for TiposDocumentos

---
+++ ClienteAPI/Models/TiposDocumento.cs
@@ -0,0 +1,10 @@
+namespace ClienteAPI.Models;
+
+public partial class TiposDocumento
+{
+    public byte IdTipoDocumento { get; set; }
+
+    public string DesTipoDocumento { get; set; } = null!;
+
+    public virtual ICollection<ClientesDocumento> ClientesDocumentos { get; } = new List<ClientesDocumento>();
+}
